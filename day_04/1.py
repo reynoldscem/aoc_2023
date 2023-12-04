3efe5ae0d848e6e151b3983a9962a12d603ea9e7
Charles EM Reynolds <reynoldscem@gmail.com>
2023-12-04 12:20:45 +0000

    Day 4 part 1

--- day_04/1.py
+++ day_04/1.py
@@ -1,4 +1,36 @@
 from pathlib import Path
+import re
+
+
+class Card:
+    def __init__(self, line):
+        card_index_substring, rest = line.split(':')
+        winning_numbers_substring, played_numbers_substring = rest.split('|')
+
+        self.index = int(re.search(r'\d+', card_index_substring).group())
+        self.winning_numbers = self.get_numbers(winning_numbers_substring)
+        self.played_numbers = self.get_numbers(played_numbers_substring)
+
+        # print(self.winning_numbers)
+        # print(self.played_numbers)
+        # print()
+
+    @property
+    def points(self):
+        intersection = self.winning_numbers.intersection(self.played_numbers)
+
+        if intersection:
+            score = 2 ** (len(intersection) - 1)
+        else:
+            score = 0
+
+        # print(score)
+
+        return score
+
+    @staticmethod
+    def get_numbers(numbers_string):
+        return set(map(int, re.findall(r'\d+', numbers_string)))
 
 
 def main():
@@ -6,7 +38,13 @@ def main():
     data_file_path = data_directory / '1.txt'
     data_string = data_file_path.read_text()
     input_lines = data_string.splitlines()
-    input_lines
+
+    print(
+        sum(
+            Card(line).points
+            for line in input_lines
+        )
+    )
 
 
 if __name__ == '__main__':
